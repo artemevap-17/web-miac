3f5fd1210cce53a61acce4c6425059c8475fdfda
Павлова Алина <info@miac74.ru>
2024-11-02 10:21:47 +0500

    Добавлен index.php

--- wp-content/themes/miac/front-page.php
+++ wp-content/themes/miac/front-page.php
@@ -231,28 +231,9 @@
             </div>
         </div>
         <div class="form">
-            <form class="feedback-form">
-                <div class="form-inputs">
-                    <div class="mb-3">
-                        <label for="inputFIO" class="form-label">ФИО</label>
-                        <input type="text" class="form-control" id="inputFIO" placeholder="ФИО" aria-describedby="FIOHelp">
-                        <div id="FIOHelp" class="form-text">Введите ФИО</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="inputEmail" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Почтовый адрес" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">Введите свой почтовый адрес для связи</div>
-                    </div>
-                </div>
-                <div class="mb-3">
-                    <label for="formControlTextarea" class="form-label">Сообщение</label>
-                    <textarea class="form-control" id="formControlTextarea" rows="3" aria-describedby="messHelp"></textarea>
-                    <div id="messHelp" class="form-text">Введите ваше сообщение</div>
-                </div>
-                <button type="submit" class="btn btn-dark">Отправить</button>
-            </form>
+                <?php echo do_shortcode('[contact-form-7 id="cd877ae" title="Контактная форма"]')?>
         </div>
-    </div>
+</div>
 </div>
 <?php get_footer(); ?>
 </body>
